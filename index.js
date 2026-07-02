1787ffbbab9b3af28974db263fdf1163d1357e74
vivekdev10 <vivekpayaal.dev@gmail.com>
2026-07-02 10:37:47 +0530

    i have just merged feat/a branch to main

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function add(a, b) {
     return a + b
 }
 
+function subtract(a, b) {
+    if (!(a > b)) throw new Error('A must be greater than b')
+    return a - b
+}
+
 add(10, 5)
 add(10, 10)
 add(50, 50)
+
+subtract(10, 5)
