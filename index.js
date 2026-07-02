1002ad2fdb4f0a98c3d051405f5e02f9521751ca
vivekdev10 <vivekpayaal.dev@gmail.com>
2026-07-02 11:02:01 +0530

    Feat/multiply (#1)
    * add multiply function to index.js
    
    * add function call to multiply in index.js
    
    * add additional multiply function calls in index.js
    
    * add another multiply function call
    
    * add duplicate multiply function call in index.js
    
    * add input validation to multiply function in index.js

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ function subtract(a, b) {
     return a - b
 }
 
+function multiply(a, b) {
+    if (a < 0 || b < 0) throw new Error('Provide no is in minus')
+    return a * b
+}
+
 add(10, 5)
 add(10, 10)
 add(50, 50)
 
 subtract(10, 5)
+
+multiply(2, 3)
+multiply(10, 5)
+multiply(20, 5)
+multiply(20, 5)
+
